c367f4e41fba3f12351d6528bd45a874cc4f7aec
ajithk-testwork <ajithk102002@gmail.com>
2026-04-06 13:19:17 +0530

    update cors file

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ const app = express();
 // middleware
 app.use(
   cors({
-    origin: ["https://shop-sphere-mern-stock-e-commerce.vercel.app",
+    origin: ["https://shop-sphere-mern-stock-e-commerce-s.vercel.app",
             "http://localhost:5173"],
     credentials: true,
   })
