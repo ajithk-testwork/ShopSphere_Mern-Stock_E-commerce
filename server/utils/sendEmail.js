dc1cbc0d7578779f1e1112bc9801505cfdbe2fee
ajithk-testwork <ajithk102002@gmail.com>
2026-04-06 15:06:02 +0530

    update emailsend in resent package

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,6 +1,7 @@
 import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
+console.log("RESEND KEY:", process.env.RESEND_API_KEY);
 
 const sendEmail = async (to, subject, text) => {
   try {
