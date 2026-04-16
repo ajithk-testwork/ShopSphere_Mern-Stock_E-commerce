ebabec74ab65d1c77d0cb2cc1ba99c018aed2b97
ajithk-testwork <ajithk102002@gmail.com>
2026-04-16 14:28:01 +0530

    payment controller update6

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -1,4 +1,4 @@
-import Order from "../models/Order.js ";
+import Order from "../models/Order.js";
 import Cart from "../models/Cart.js";
 
 
@@ -22,10 +22,29 @@ export const placeOrder = async (req, res) => {
 
   const order = await Order.create({
     user: req.user._id,
-    items: cart.items,
+
+    // ✅ USER SNAPSHOT
+    userInfo: {
+      name: req.user.name,
+      email: req.user.email,
+    },
+
+    // ✅ PRODUCT SNAPSHOT
+    items: cart.items.map((item) => ({
+      product: item.product._id,
+      name: item.product.name,
+      image: item.product.image,
+      price: item.product.price,
+      quantity: item.quantity,
+    })),
+
     shippingAddress,
     totalAmount,
   });
+
+  // ✅ clear cart
+  cart.items = [];
+  await cart.save();
 
   res.status(201).json(order);
 };
@@ -34,9 +53,6 @@ export const placeOrder = async (req, res) => {
   // GET USER ORDERS
 
 export const getMyOrders = async (req, res) => {
-  const orders = await Order.find({ user: req.user._id }).populate(
-    "items.product"
-  );
-
+  const orders = await Order.find({ user: req.user._id });
   res.json(orders);
 };
